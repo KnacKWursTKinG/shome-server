3ca8740511085b83bd6b71170e294e7a8fc807a2
KnacKWursTKinG <bangba33@hotmail.com>
2021-05-06 21:26:15 +0200

    remove store with pickle (build module)

--- client/nmpvc/scripts/mympv.py
+++ client/nmpvc/scripts/mympv.py
@@ -1,6 +1,122 @@
+#!/usr/bin/env python
 
-import pickle
+from __future__ import annotations
+
+import threading
+import inspect
+import pprint
+import types
+
+import mpv
+
+from kwking_helper.thread import threaded2, ThreadData
+from kwking_helper.logging import CL
 
 
-with open('mpv.pickle', 'rb') as file:
-    MPV = pickle.load(file)
+logger = CL(
+    'debug', __name__, _file='mympv.log',
+    _format="[{level}] {message}",
+    _format_file="[{level}] {message}",
+    _file_force_color=True
+)
+
+
+class MPV:
+    def _run(self, method: str, *args, **kwargs):
+        print(f'[_run] {method=}, {args=}, {kwargs=}')
+
+    def _set(self, prop: str, value):
+        print(f'[_set] {prop=}, {value=}')
+
+    def _get(self, prop: str):
+        print(f'[_get] {prop=}')
+        ...
+
+    def new(self, *flags, **player_args):
+        ...
+
+    def quit(self):
+        self._run('quit')
+
+    def terminate(self):
+        self.quit()
+
+
+class BuildClass:
+    Queue: list[BuildClass] = list()
+    ThreadLock = threading.Lock()
+
+    def __init__(self, class_obj):
+        self.class_obj = class_obj
+
+        self.__thread = None
+
+    def run(self, obj, name):
+        attr = getattr(obj, name)
+
+        if isinstance(attr, types.MethodType):
+            # @todo: get args and kwargs via inspect module and add to class
+            params = inspect.getfullargspec(attr)
+            ...
+
+            with BuildClass.ThreadLock:
+                if params.varargs and params.varkw:
+                    logger.debug(f"[add method] player.{name}({', '.join(list(map(str, params.args)))}, *{params.varargs}, **{params.varkw})")
+                elif params.varargs:
+                    logger.debug(f"[add method] player.{name}({', '.join(list(map(str, params.args)))}, *{params.varargs})")
+                elif params.varkw:
+                    logger.debug(f"[add method] player.{name}({', '.join(list(map(str, params.args)))}, **{params.varkw})")
+                else:
+                    logger.debug(f"[add method] player.{name}({', '.join(list(map(str, params.args)))})")
+
+                if params.kwonlyargs:
+                    logger.debug(f"[add method] ... {params.kwonlyargs=}")
+                    logger.debug(f"[add method] ... {params.kwonlydefaults=}")
+
+        else:
+            with BuildClass.ThreadLock:
+                setattr(self.class_obj, name, property(
+                    lambda self: self._get(name),
+                    lambda self, value: self._set(name, value)
+                ))
+
+                logger.debug(f"[add property] {name}")
+                logger.debug(f"[add property] ... defalut:\n{pprint.pformat(getattr(obj, name))}")
+
+    def start(self, obj, attr):
+        if self.__thread is not None:
+            logger.critical(f"[{self.__thread.thread.name}] Thread already started!")
+            return
+
+        @threaded2(True, on_success, on_error)
+        def _run(self, obj, attr):
+            self.run(obj, attr)
+
+        self.__thread = _run(self, obj, attr)
+
+    @classmethod
+    def wait_for_finish(cls):
+        while (_thread := BuildClass.Queue.pop(0) if BuildClass.Queue else None):
+            _thread.join()
+            del _thread
+
+
+def on_success(td: ThreadData, ret):
+    pass
+
+
+def on_error(td: ThreadData, err):
+    with BuildClass.ThreadLock:
+        logger.error(f"[{td.thread.name}] [on_error] {err!r}")
+
+
+_player = mpv.MPV()
+
+for _attr in dir(_player):
+    if _attr[0] == '_':
+        continue
+
+    BuildClass(MPV).start(_player, _attr)
+
+del _player
+BuildClass.wait_for_finish()
