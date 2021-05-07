0618dd7c8cdc7902c0a7ffc2a9fd5ae6ec4abc93
KnacKWursTKinG <bangba33@hotmail.com>
2021-05-07 17:18:35 +0200

    base class generator

--- client/nmpvc/nmpvc/base.py
+++ client/nmpvc/nmpvc/base.py
@@ -1,13 +1,1 @@
-# @todo: ...
-#   * get nmpv.player.Player() and mpv.MPV() attr
-#   * create method and attr list with `inspect.getfullargspec(...)`
-#       > the return will be from type `inspect.FullArgSpec`
-
-
-class NetMPVControlHelp:
-    # @todo: get method params: `inspect.getfullargspec(getattr(MPV(), 'playlist_append'))`
-    pass
-
-
-class NetMPVControlBase:
-    pass
+""" Will be generatet from script: '../scripts/build-mpv.py' """
--- client/nmpvc/scripts/build-mpv.py
+++ client/nmpvc/scripts/build-mpv.py
@@ -1,24 +1,54 @@
 
 import types
+import inspect
 
 from kwking_helper.logging import CL
 
 
 Code = """
 
+import json
+
+import requests
+
+from kwking_helper import rq
+
+
 class MPV:
-    def __init__(self):
-        ...
+    def __init__(self, host, port = 50870):
+        self.url = f"http://{socket.gethostbyname(host)}:{port}/api/nmpv/player"
+
+    def _send_data(self, data):
+        resp = requests.post(
+            self.url,
+            json.loads(data),
+            headers={
+                'Content-Type': 'application/json'
+            }
+        )
+
+        if resp.status_code != 200:
+            raise rq.RQError(resp)
+
+        return json.loads(resp.text) if "application/json" in resp.headers.get('Content-Type') else None
 
     def _run_method(self, name: str, *args, **kwargs):
-        ...
+        return self._send_data({
+            "attr": str(name),
+            "args": args,
+            "kwargs": kwargs
+        })
 
     def _set_prop(self, prop: str, value):
-        ...
+        return self._send_data({
+            "attr": str(prop),
+            "value": value
+        })
 
     def _get_prop(self, prop: str):
-        ...
-
+        return self._send_data({
+            "attr": str(prop)
+        })
 """
 
 TemplateProperty = """
@@ -29,16 +59,19 @@ TemplateProperty = """
 
     @{name}.setter
     def {name}(self, value):
-        self._set_prop('{name}', value)
+        return self._set_prop('{name}', value)
+"""
 
+TemplateMethod = """
+
+    def {name}({args}, *{varargs}, **{varkw}):
+        return self._run_method({name}, {args}, *{varargs}, **{varkw})
 """
 
 
 
 class BuildMPV:
-    def __init__(self, out: str):
-        self.out = out
-
+    def __init__(self):
         self.logger = CL(
             'debug', __name__, _format="[{level}] {message}"
         )
@@ -60,20 +93,28 @@ class BuildMPV:
             if name[0] == '-':
                 continue
 
+            if name in ['quit', 'terminate']:
+                continue
+
             if isinstance(attr, types.MethodType):
-                # @todo: generate method
-                ...
+                _params = inspect.getfullargspec(attr)
+
+                self.code += TemplateMethod.format(
+                    name=name,
+                    args=', '.join(_params.args),
+                    varargs=_params.varargs or 'args',
+                    varkw=_params.varkw or 'kwargs'
+                )
 
             else:
                 self.code += TemplateProperty.format(name=name)
 
-    def save(self):
-        # @todo: write to file
-        with open(self.out, 'w') as file:
+    def save(self, out: str):
+        with open(out, 'w') as file:
             file.write(self.code)
 
 
 if __name__ == "__main__":
-    build = BuildMPV('nmpvc.py')
+    build = BuildMPV()
     build.run()
-    build.save()
+    build.save('nmpvc.py')
