ee4f724dcd4eda1b3729460fd8fbbe630faa6705
KnacKWursTKinG <bangba33@hotmail.com>
2021-05-09 06:43:59 +0200

    some simple help commands for search in mpv.MPV attributes ...

--- client/nmpvc/nmpvc/__main__.py
+++ client/nmpvc/nmpvc/__main__.py
@@ -7,4 +7,15 @@ import click
 module_path = os.path.abspath(__file__).rsplit('/', 2)[0]
 sys.path.insert(0, module_path)
 
-...
+from nmpvc._click import help
+
+
+@click.group()
+def cli():
+    """ Network MPV Client """
+
+
+cli.add_command(help.cli)
+
+
+cli()
---
+++ client/nmpvc/nmpvc/_click/help.py
@@ -0,0 +1,58 @@
+
+import json
+import pprint
+
+import click
+import click_aliases
+
+
+@click.group('help', cls=click_aliases.ClickAliasedGroup)
+@click.pass_context
+def cli(ctx):
+    """ search for methods or attributes in `mpv.MPV` """
+    with open(__file__.rsplit('/', 1)[0] + '/mpv-help.json', 'r') as file:
+        ctx.obj = json.load(file)
+
+
+@cli.command('search')
+@click.option('-s', '--short', is_flag=True, default=False, help="only show matching property/method name(s)")
+@click.argument('name')
+@click.pass_obj
+def search(obj, short: bool, name: str):
+    """ Search for an attribute """
+    matches = list()
+    for _key, _value in obj['method'].items():
+        if name in _key:
+            matches.append((_key, _value))
+
+    for _key, _value in obj['property'].items():
+        if name in _key:
+            matches.append((_key, _value))
+
+    for idx, (_name, _data) in enumerate(matches):
+        if idx > 0 and not short:
+            click.echo()
+
+        click.echo(_name)
+
+        if not short:
+            click.echo('-' * len(_name))
+            click.echo(f"{pprint.pformat(_data)}")
+
+
+@cli.command('info')
+@click.argument('name')
+@click.pass_obj
+def info(obj, name: str):
+    """ get info for attribute """
+    for _key in obj['method']:
+        if _key == name:
+            click.echo(_key)
+            click.echo('-' * len(_key))
+            click.echo(f"{pprint.pformat(obj['method'][_key])}")
+
+    for _key in obj['property']:
+        if _key == name:
+            click.echo(_key)
+            click.echo('-' * len(_key))
+            click.echo(f"{pprint.pformat(obj['property'][_key])}")
