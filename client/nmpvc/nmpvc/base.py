4f24602a00de606eb5f234dd6c79175a1fda57c6
KnacKWursTKinG <bangba33@hotmail.com>
2021-05-12 09:56:58 +0200

    clean up & new mpv base class

--- client/nmpvc/nmpvc/base.py
+++ client/nmpvc/nmpvc/base.py
@@ -1,7 +1,7 @@
 
 import json
 
-from typing import Any
+from typing import Any, Union
 
 import requests
 
@@ -9,23 +9,34 @@ from kwking_helper import rq  # type: ignore
 from kwking_helper.thread import threaded2, ThreadData  # type: ignore
 
 
-class MPVError(Exception):
-    def __init__(self, status_code: int, *server):
-        super().__init__(f"[{status_code}] {', '.join(server)}")
-        self.status_code = status_code
-        self.server = server
+class MPV:
+    def __init__(self, *addr: tuple[str, int], sync: bool = True):
+        self._port = 50870
+        self.sync = bool(sync)
+        self.addr = list(addr)
 
+    @property
+    def addr(self) -> list[tuple[str, int]]:
+        return self._addr
 
-class MPVBase:
-    def __init__(self, port: int = 50870):
-        self.port = port
+    @addr.setter
+    def addr(self, addr: list[Union[str, tuple[str, int]]]):
+        self._addr = list()
 
-    def url(self, server: str):
-        return f"http://{server}:{self.port}/api/nmpv/player"
+        for _addr in addr:
+            if isinstance(_addr, str):
+                self._addr.append((_addr, self._port))
+            elif isinstance(_addr, tuple):
+                self._addr.append((_addr[0], _addr[1]))
+            else:
+                raise ValueError("'addr' should contain tuples with host and port")
 
-    def send(self, server: str, data: Any):
+    def _url(self, addr: tuple[str, int]):
+        return "http://{}:{}/api/nmpv/player".format(*addr)
+
+    def _send(self, addr: tuple[str, int], data: Any):
         resp = requests.post(
-            self.url(server),
+            self._url(addr),
             json.loads(data),
             headers={
                 'Content-Type': 'application/json'
@@ -40,53 +51,40 @@ class MPVBase:
 
         return None
 
-    def run(self, server: str, name: str, *args, **kwargs):
-        return self.send(
-            server,
+    @threaded2(daemon=True)
+    def _run(self, addr: tuple[str, int], name: str, *args, **kwargs):
+        return self._send(
+            addr,
             {
+                "sync": ...,  # @todo
                 "attr": str(name),
                 "args": args,
                 "kwargs": kwargs
             }
         )
 
-    def set(self, server: str, prop: str, value: Any):
-        return self.send(
-            server,
+    @threaded2(daemon=True)
+    def _set(self, addr: tuple[str, int], prop: str, value: Any):
+        return self._send(
+            addr,
             {
+                "sync": ...,  # @todo
                 "attr": str(prop),
                 "value": value
             }
         )
 
-    def get(self, server: str, prop: str):
-        return self.send(
-            server,
+    @threaded2(daemon=True)
+    def _get(self, addr: tuple[str, int], prop: str):
+        return self._send(
+            addr,
             {
+                "sync": ...,  # @todo
                 "attr": str(prop)
             }
         )
 
-
-class MPV:
-    def __init__(self, *addr: tuple[str, int]):
-        self.base: dict[str, MPVBase] = dict()
-
-        self.addr = list(addr)
-
-    @property
-    def addr(self):
-        return [(host, self.base[host].port) for host in self.base]
-
-    @addr.setter
-    def addr(self, addr: list[tuple[str, int]]):
-        for server, port in addr:
-            if server in self.base:
-                del self.base[server]
-
-            self.base[server] = MPVBase(int(port))
-
-    def _threads(self, threads: list[tuple[str, ThreadData]]):
+    def _threads(self, threads: list[tuple[tuple[str, int], ThreadData]]):
         ret = list()
         for server, _thread in threads:
             try:
@@ -94,31 +92,21 @@ class MPV:
             except Exception as ex:
                 ret.append((server, ex))
 
+            del _thread
+
         return ret
 
     def run(self, name: str, *args, **kwargs) -> list[tuple[str, Any]]:
-        @threaded2(daemon=True)
-        def _run(server: str, base: MPVBase):
-            return base.run(server, name, *args, **kwargs)
-
         return self._threads(
-            [(server, _run(server, base)) for server, base in self.base.items()]
+            [(addr, self._run(addr, name, *args, **kwargs)) for addr in self.addr]
         )
 
     def set(self, name: str, value: Any):
-        @threaded2(daemon=True)
-        def _set(server: str, base: MPVBase):
-            return base.set(server, name, value)
-
         return self._threads(
-            [(server, _set(server, base)) for server, base in self.base.items()]
+            [(addr, self._set(addr, name, value)) for addr in self.addr]
         )
 
     def get(self, name: str):
-        @threaded2(daemon=True)
-        def _get(server: str, base: MPVBase):
-            return base.get(server, name)
-
         return self._threads(
-            [(server, _get(server, base)) for server, base in self.base.items()]
+            [(addr, self._get(addr, name)) for addr in self.addr]
         )
