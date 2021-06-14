beef3f2a10e9be3e2d1b43e9ae5a68e3e387a50b
KnacKWursTKinG <bangba33@hotmail.com>
2021-06-14 14:37:39 +0200

    remove client

--- plugins/all/nmpv/.client/nmpvc/stream.py
+++ plugins/all/nmpv/.client/nmpvc/stream.py
@@ -15,44 +15,37 @@ from flask import Flask, Response, request
 from kwking_helper.logging import CL
 
 
-__all__ = ['Stream']
+class StreamLocal(Process):
+    Queue: set[StreamLocal] = set()
 
-
-# TODO: add support for multiple hosts (quit only after last request in hosts)
-# TODO: add support for youtube
-class Stream(Process):
-    Queue: set[Process] = set()
-
-    def __init__(self, file: str, hosts: Union[str, list[str]], log_level: str = 'warning'):
+    def __init__(self, file: str, hosts: list[str], log_level: str = 'warning'):
         super().__init__()
         self.daemon = True
         self.id = id(self)
         self.name = f"stream-{self.id}"
 
         self.file = os.path.expanduser(file)
+        self.hosts: list[str] = [socket.gethostbyname(_) for _ in list(set(hosts))]
 
         if not os.path.isfile(self.file):
             raise FileNotFoundError(self.file)
-
-        self.mimetype = mimetypes.guess_type(self.file)[0]
-        self.rule = f"/{self.id}"
-
-        _hosts = set([hosts] if not isinstance(hosts, list) else hosts)
-        self.hosts: list[str] = [socket.gethostbyname(_) for _ in _hosts]
-        self.host = '0.0.0.0' if len(self.hosts) > 1 else self.hosts[0]
-        self.port = 0
-        self.chunk_size = 1024 * 1024
 
         self.logger = CL(
             log_level, f"{self.name}"
         )
 
+        self._chunk_size = 1024 * 1024
+        self._mimetype = mimetypes.guess_type(self.file)[0]
+        self._rule = f"/{self.id}"
+        self._host = '0.0.0.0'
+        self._port = 0
+
     def __del__(self):
         if self.is_alive():
             self.kill()
 
-        if self in Stream.Queue:
-            Stream.Queue.remove(self)
+        if self in StreamLocal.Queue:
+            StreamLocal.Queue.remove(self)
 
     def __enter__(self):
         self.start()
@@ -64,59 +57,59 @@ class Stream(Process):
         self.__del__()
 
     def start(self):
-        self.generate_port()
+        self._generate_port()
         super().start()
-        Stream.Queue.add(self)
+        StreamLocal.Queue.add(self)
 
     @property
     def url(self):
-        return f"http://{self.host}:{self.port}{self.rule}"
+        return f"http://{self._host}:{self._port}{self._rule}"
 
-    def generate_port(self):
+    def _generate_port(self):
         with socket.create_server(('localhost', 0)) as s:
-            self.port = s.getsockname()[1]
+            self._port = s.getsockname()[1]
 
     def run(self):
         import logging
-        server = Flask(self.name)
+
         self.hosts_server_quit = self.hosts
+
+        server = Flask(self.name)
+
+        # disable flask logging stuff
         logging.getLogger('werkzeug').disabled = True
         os.environ['WERKZEUG_RUN_MAIN'] = 'true'
 
         @server.after_request
         def quit(response: Response):
             try:
-                self.logger.debug(
-                    f"remove {request.remote_addr!r} from {self.hosts_server_quit!r}"
-                )
                 self.hosts_server_quit.remove(request.remote_addr)
-            except ValueError:
+            except ValueError as ex:
                 if request.remote_addr == '127.0.0.1':
-                    self.logger.debug(
-                        f"ValueError: remove '127.0.1.1' from {self.hosts_server_quit!r}"
-                    )
                     self.hosts_server_quit.remove('127.0.1.1')
+                else:
+                    raise ex
 
             def _exit():
                 time.sleep(1)
                 os._exit(0)
 
             if not self.hosts_server_quit:
-                self.logger.debug(f"quit server ({self.host=}, {self.port=})")
+                self.logger.debug(f"quit server: {self.url}")
                 Thread(target=_exit).start()
 
             return response
 
-        @server.route(self.rule)
+        @server.route(self._rule)
         def stream():
             def generator():
                 with open(self.file, 'rb') as f:
-                    while (data := f.read(self.chunk_size)):
+                    while (data := f.read(self._chunk_size)):
                         yield data
 
             return Response(
-                generator(), mimetype=self.mimetype
+                generator(), mimetype=self._mimetype
             )
 
-        self.logger.debug(f"server start on ({self.host=}, {self.port=})")
-        server.run(self.host, self.port)
+        self.logger.debug(f"server started: {self.url}")
+        server.run(self._host, self._port)
